5fc17d9064af646f747b40fc29889923c3cd98ce
kings177 <yanmendesp@gmail.com>
2023-01-24 01:06:02 -0300

    parallel code added for Bench on V8 and HVM

--- poctestV8.bash
+++ poctestV8.bash
@@ -1,4 +1,11 @@
-for i in {1..10}
+for i in {1..5}
 do
 	time node main.js
+done &
+
+for i in {1..5}
+do
+        time node main.js
 done
+
+
--- poctesthvm.bash
+++ poctesthvm.bash
@@ -1,4 +1,9 @@
-for i in {1..10}
+for i in {1..5}
 do
 	cargo run --release -- run -t 4 -c true "(Main 0)"	
+done &
+
+for i in {1..5}
+do
+        cargo run --release -- run -t 4 -c true "(Main 0)"
 done
