779b85ad1eddbb469e5d6b4c9bbee3450f639409
kings177 <yanmendesp@gmail.com>
2023-11-22 04:52:27 -0300

    fix: finally actually fixed now

--- GithubAPI/fetch_threading.py
+++ GithubAPI/fetch_threading.py
@@ -3,21 +3,15 @@ import json
 import time
 
 def load_repos(filename):
-    """ Load repository data from JSON file"""
     with open(filename, 'r') as file:
         return json.load(file)
 
-def get_rate_limit(api_token):
-    """ Check the current rate limit status"""
-    headers = {'Authorization': f'token {api_token}'}
-    response = requests.get('https://api.github.com/rate_limit', headers=headers)
-    if response.status_code == 200:
-        rate_limit = response.json()['resources']['search']
-        return rate_limit['remaining'], rate_limit['reset']
-    return 0, 0
+def get_rate_limit_from_headers(headers):
+    remaining = int(headers.get('X-RateLimit-Remaining', 0))
+    reset = int(headers.get('X-RateLimit-Reset', 0))
+    return remaining, reset
 
 def wait_for_rate_limit_reset(reset_time):
-    """ Wait until the rate limit is reset"""
     wait_time = reset_time - time.time()
     if wait_time > 0:
         print(f"Rate limit exceeded. Sleeping for {wait_time} seconds")
@@ -27,30 +21,25 @@ def wait_for_rate_limit_reset(reset_time):
 api_call_counter = 0
 
 def search_in_repo(repo_url, keyword, api_token):
-    """ Search for a keyword in a repository """
     global api_call_counter
+    api_call_counter += 1
 
     headers = {'Authorization': f'token {api_token}'}
     repo_full_name = repo_url.replace('https://github.com/', '')
     query = f"{keyword}+repo:{repo_full_name}"
     url = f"https://api.github.com/search/code?q={query}"
 
-    api_call_counter += 1
-
-    if api_call_counter % 25 == 0:
-        remaining, reset = get_rate_limit(api_token)
-        if remaining <= 1:
-            wait_for_rate_limit_reset(reset)
-
-    print(f"Searching for {keyword} in {repo_url}, Remaining requests before call: {remaining}")
-
     response = requests.get(url, headers=headers)
 
-    remaining_after, _ = get_rate_limit(api_token)
-    print(f"Remaining requests after call: {remaining_after}")
+    remaining, reset = get_rate_limit_from_headers(response.headers)
+    print(f"Rate limit status (from header): {remaining} requests remaining")
+    if remaining <= 1:
+        wait_for_rate_limit_reset(reset)
 
-    # print(f"Response: {response.json()}")
-    time.sleep(1)
+    if response.status_code == 200 and response.json()['total_count'] > 0:
+        print(f"Found {keyword} in {repo_url}")
+    else:
+        print(f"No {keyword} found in {repo_url}")
 
     return response.status_code == 200 and response.json()['total_count'] > 0
 
